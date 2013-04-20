d108cf1179bd6297acdbe78fc2e0ee4cec83e74f
Teddy Zeenny <teddyzeenny@gmail.com>
2013-04-20 20:32:09 +0300

    precompile

---
+++ public/assets/app/templates/application-48beb2c32ea480387010e785d2f8e909.js
@@ -0,0 +1,1 @@
+Ember.TEMPLATES.application=Ember.Handlebars.template(function(e,a,r,s,t){this.compilerInfo=[2,">= 1.0.0-rc.3"],r=r||Ember.Handlebars.helpers,t=t||{};var h,p="",n=this.escapeExpression;return t.buffer.push("<h1>Ember Scaffold</h1>\n"),h={},t.buffer.push(n(r._triageMustache.call(a,"outlet",{hash:{},contexts:[a],types:["ID"],hashTypes:h,data:t}))),t.buffer.push("\n"),p});
